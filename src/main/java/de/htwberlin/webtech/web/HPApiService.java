80405c4005fa8862dd4bdafd93f6128c0c13e06c
Louris Marianna Houch <Louris-Marianna.Houch@Student.HTW-Berlin.de>
2025-05-10 12:34:13 +0200

    test-yml

--- src/main/java/de/htwberlin/webtech/web/HPApiService.java
+++ src/main/java/de/htwberlin/webtech/web/HPApiService.java
@@ -1,4 +1,5 @@
-package de.htwberlin.webtech.web;
+
+/**
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
@@ -41,3 +42,46 @@ public class HPApiService {
 
 
 }
+ **/
+package de.htwberlin.webtech.web;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+@Service
+public class HPApiService {
+
+    private final RestTemplate restTemplate;
+    private static final String BASE_URL = "https://hp-api.onrender.com/api";
+
+    public HPApiService() {
+        this.restTemplate = new RestTemplate();
+    }
+
+    public String getAllCharacters() {
+        ResponseEntity<String> response = restTemplate.getForEntity(BASE_URL + "/characters", String.class);
+        return response.getBody();
+    }
+
+    public String getStudents() {
+        ResponseEntity<String> response = restTemplate.getForEntity(BASE_URL + "/characters/students", String.class);
+        return response.getBody();
+    }
+
+    public String getSpells() {
+        ResponseEntity<String> response = restTemplate.getForEntity(BASE_URL + "/spells", String.class);
+        return response.getBody();
+    }
+
+    public String getHogwartsStaff() {
+        String url = BASE_URL + "/characters/staff";
+        return restTemplate.getForObject(url, String.class);
+    }
+
+    public String getCharactersByHouse(String house) {
+        String url = BASE_URL + "/characters/house/" + house;
+        return restTemplate.getForObject(url, String.class);
+    }
+}
+
