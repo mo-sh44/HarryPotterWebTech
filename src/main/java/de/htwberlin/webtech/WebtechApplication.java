54a0bedc47f5b679278ebc19567d8ae21d0a10fa
mo-sh44 <mohamad-shahin.94@hotmail.com>
2025-06-18 02:37:40 +0200

    fix verbindung mit front

--- src/main/java/de/htwberlin/webtech/WebtechApplication.java
+++ src/main/java/de/htwberlin/webtech/WebtechApplication.java
@@ -2,14 +2,13 @@ package de.htwberlin.webtech;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import io.github.cdimascio.dotenv.Dotenv;
 
 
 @SpringBootApplication
 public class WebtechApplication {
 	public static void main(String[] args) {
+		Dotenv dotenv = Dotenv.configure().ignoreIfMissing().load();
 		SpringApplication.run(WebtechApplication.class, args);
 	}
 }
-
-
-
--- src/main/java/de/htwberlin/webtech/web/WebConfig.java
+++ src/main/java/de/htwberlin/webtech/web/WebConfig.java
@@ -2,16 +2,19 @@ package de.htwberlin.webtech.web;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
+@EnableWebMvc
 public class WebConfig implements WebMvcConfigurer {
+
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/api/**")  // nur eigene API-Routen erlauben
-                .allowedOrigins("https://harrypotter-frontend.onrender.com")
-                .allowedMethods("GET", "POST", "PUT", "DELETE")
+        registry.addMapping("/**")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedOrigins("http://localhost:5174","http://localhost:5175", "https://harrypotter-frontend.onrender.com" )
                 .allowedHeaders("*")
-                .allowCredentials(true); // falls Login oder Cookies nötig
+                .allowCredentials(true);
     }
 }
